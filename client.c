fed16ab89b5b6becd0d79e323b0743140d374c8a
Vebstere <mittalvaibhav277@gmail.com>
2024-05-14 13:24:01 +0530

    changed client.c

--- client.c
+++ client.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include"string.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 //client
+
+
 int main(){
     struct sockaddr_in serv;
     int sd = socket(AF_INET,SOCK_STREAM,0);
@@ -13,9 +16,102 @@ int main(){
     serv.sin_port = htons(5000);
     connect(sd,(struct sockaddr *)&serv,sizeof(serv));
     perror("Connection with server");
-    char buf[100];
-    write(sd,"This is msg from client\n",sizeof("This is msg from client\n"));
-    read(sd,buf,sizeof(buf));
-    printf("%s\n",buf);
+
+     char username[100];
+    char password[100];
+    char auth_message[100];
+
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    // Send username to server
+    send(sd, username, strlen(username), 0);
+
+    // Send password to server
+    send(sd, password, strlen(password), 0);
+
+    // Receive authentication result from server
+    recv(sd, auth_message, sizeof(auth_message), 0);
+    printf("%s\n", auth_message);
+    if (strcmp(auth_message, "Authenticated") == 0) {
+        printf("Authentication successful!\n");
+
+        while(1){
+            printf("Welcome to Library Management System \n");
+            printf("Enter your choice: \n");
+            printf("A- Issue book \n");
+            printf("B- Return book \n");
+            printf("C- Exit \n");
+            char choice;
+            scanf("%c", choice);
+            if(choice == 3){
+                break;
+            }
+            send(sd,choice, sizeof(choice), 0);
+
+            switch (choice)
+            {
+            case 'A':
+                issueBook(sd);
+                break;
+
+            case 'B':
+                returnBook(sd);
+                break;
+            
+            default:
+                break;
+            }
+
+        }
+
+        
+    }
+    else{
+        printf("Authentication failed!\n");
+        exit(0);
+    }
+
     close(sd);
+
+    // char buf[100];
+    // write(sd,"This is msg from client\n",sizeof("This is msg from client\n"));
+    // read(sd,buf,sizeof(buf));
+    // printf("%s\n",buf);
+    // close(sd);
+}
+
+void issueBook(int sd){
+    char bookName[100];
+    char author[100];
+    int bookId;
+
+    printf("Enter book name: ");
+    scanf("%s", bookName);
+
+    printf("Enter author name: ");
+    scanf("%s", author);
+
+    printf("Enter book id: ");
+    scanf("%d", &bookId);
+
+    send(sd, bookName, sizeof(bookName), 0);
+    send(sd, author, sizeof(author), 0);
+    send(sd, &bookId, sizeof(bookId), 0);
+    char result[100];
+    recv(sd, result, sizeof(result), 0);
+    printf("%s\n", result);
+}
+
+void returnBook(int sd){
+    int bookId;
+    printf("Enter book id to return: ");
+    scanf("%d", &bookId);
+    send(sd, bookId, sizeof(bookId), 0);
+
+    char result[100];
+    recv(sd, result, sizeof(result), 0);
+    printf("%s\n", result);
 }
--- server.c
+++ server.c
@@ -1,26 +1,96 @@
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
-//server, run first
-int main(){
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAX_CLIENTS 5
+
+
+typedef struct{
+    char name[100];
+    char password[100];
+    int mmid;
+
+} Member;
+
+Member members[2]={
+    {"admin","admin",1},
+    {"user","user",2}
+};
+
+void authenticate(int client_socket){
+    char name[100];
+    char password[100];
+    int authenticated=0;
+
+    recv(client_socket, name, sizeof(name), 0);
+
+    // Receive password from client
+    recv(client_socket, password, sizeof(password), 0);
+
+
+    for(int i=0;i<2;i++){
+        if(strcmp(name,members[i].name)==0 && strcmp(password,members[i].password)==0){
+            authenticated=1;
+            break;
+        }
+    }
+    if(authenticated){
+        send(client_socket, "Authenticated", sizeof("Authenticated"), 0);
+    }
+    else{
+        send(client_socket, "Not Authenticated", sizeof("Not Authenticated"), 0);
+    }
+
+}
+
+
+void *handleconnect(void *nsd) {
+    int sd = *((int *)nsd);
+    char buf[1000];
+    authenticate(sd);
+    read(sd, buf, 1000);
+    printf("Message from client: %s", buf);
+    write(sd, "Hello, from server\n", sizeof("Hello, from server\n"));
+    close(sd);
+    pthread_exit(NULL);
+}
+
+int main() {
     struct sockaddr_in serv, cli;
-    int sd;
-    sd = socket(AF_INET, SOCK_STREAM, 0);
+    pthread_t threads[MAX_CLIENTS];
+    int i=0;
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    serv.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = INADDR_ANY;
     serv.sin_port = htons(5000);
     bind(sd, (struct sockaddr *)&serv, sizeof(serv));
-    listen(sd, 0);
-    int nsd;
-    int len = sizeof(cli);
-    nsd = accept(sd, (struct sockaddr *)&cli, (unsigned int *)&len);
-    char buf[100];
-    read(nsd, buf, sizeof(buf));
-    write(1, buf, sizeof(buf));
-    write(nsd, "This is msg from server\n", sizeof("This is msg from server\n"));
+    listen(sd, 5);
+    printf("Server listening on port 5000\n");
+    while (1) {
+        int len = sizeof(cli);
+        int nsd = accept(sd, (struct sockaddr *)&cli, &len);
+        pthread_create(&threads[i++], NULL, handleconnect, (void *)&nsd);
+        if (i >= MAX_CLIENTS)
+        {
+            i = 0;
+            // Join finished threads to release resources
+            while (i < MAX_CLIENTS)
+            {
+                pthread_join(threads[i++], NULL);
+            }
+            i = 0;
+        }
+    }
     close(sd);
-    return(0);
+    return 0;
 }
